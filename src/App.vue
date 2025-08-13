97a2564c121e5050e73dfadec07b127c8ead661f
PressureFlow <141013914+PressureFlow@users.noreply.github.com>
2025-08-13 20:54:19 +0300

    add preconnect for googlefonts, and delete output.css

--- src/App.vue
+++ src/App.vue
@@ -10,10 +10,5 @@ import NavPage from "./components/navigation/NavPage.vue";
 </template>
 
 <style>
-body {
-  font-family: "Montserrat", serif;
-}
-html, body {
-  height: calc(100% - 50px);
-}
+
 </style>
